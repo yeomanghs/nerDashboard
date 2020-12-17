0025a97f18e72b9a536c73701fabafb62969317a
yeomanghs <30331454+yeomanghs@users.noreply.github.com>
2020-12-17 16:30:23 +0800

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ for col in ['PERSON', 'ORG', 'Overall', 'NE']:
     resultDictCrf[col] = df2.query('Metric in @metricList')[col].values
     
 #Bootstrap themes
-app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP])
+app = dash.Dash(__name__, external_stylesheets = [dbc.themes.BOOTSTRAP], meta_tags=[{"name": "viewport", "content": "width=device-width"}])
 server = app.server
 
 # the style arguments for the sidebar. We use position:fixed and a fixed width
